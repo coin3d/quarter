5fe3016b80b20377aa6b2ad9fb1d0a0c87b18ba1
Lars Moastuen <larsm@sim.no>
2008-05-30 15:59:34 +0000

    Updated FindCoin.cmake

--- CMakeMacros/Modules/FindCoin.cmake
+++ CMakeMacros/Modules/FindCoin.cmake
@@ -1,12 +1,7 @@
-# I have better stuff to do than closing my
-# constructs properly
 SET(CMAKE_ALLOW_LOOSE_LOOP_CONSTRUCTS True)
 
 SET(COINDIR $ENV{COINDIR})
 
-# FIXME: Add support for static versions of Coin and merge these changes into the
-# CMakeMacros repository.
-# 2008-05-15, oyshole
 SET(COIN_NAMES Coin Coin3)
 SET(COIN_NAMES_DEBUG Coind Coin3d)
 
@@ -17,20 +12,42 @@ ENDIF(APPLE)
 
 IF(COINDIR)
   # Look for Coin in environment variable COINDIR
-  FIND_LIBRARY(Coin_LIBRARY_RELEASE NAMES ${COIN_NAMES} PATHS ${COINDIR} PATH_SUFFIXES src bin lib .)
-  FIND_LIBRARY(Coin_LIBRARY_DEBUG NAMES ${COIN_NAMES_DEBUG} PATHS ${COINDIR} PATH_SUFFIXES src bin lib .)
+  FIND_LIBRARY(Coin_LIBRARY_RELEASE NAMES ${COIN_NAMES} PATHS ${COINDIR} PATH_SUFFIXES src bin lib . NO_DEFAULT_PATH)
+  FIND_LIBRARY(Coin_LIBRARY_DEBUG NAMES ${COIN_NAMES_DEBUG} PATHS ${COINDIR} PATH_SUFFIXES src bin lib . NO_DEFAULT_PATH)
   
-  IF (CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
-    SET(Coin_LIBRARY optimized ${Coin_LIBRARY_RELEASE} debug ${Coin_LIBRARY_DEBUG})
-  ELSE (CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
-	SET(Coin_LIBRARY ${Coin_LIBRARY_RELEASE})
-  ENDIF (CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
+  IF(Coin_LIBRARY_RELEASE AND NOT Coin_LIBRARY_DEBUG)
+    SET(Coin_LIBRARY_DEBUG ${Coin_LIBRARY_RELEASE})
+    SET(Coin_LIBRARY       ${Coin_LIBRARY_RELEASE})
+    SET(Coin_LIBRARIES     ${Coin_LIBRARY_RELEASE})
+  ENDIF()
   
-  FIND_PATH(Coin_INCLUDE_DIR Inventor/SbLinear.h PATHS ${COINDIR} PATH_SUFFIXES include .)
+  IF(Coin_LIBRARY_DEBUG AND NOT Coin_LIBRARY_RELEASE)
+    SET(Coin_LIBRARY_RELEASE ${Coin_LIBRARY_DEBUG})
+    SET(Coin_LIBRARY       ${Coin_LIBRARY_DEBUG})
+    SET(Coin_LIBRARIES     ${Coin_LIBRARY_DEBUG})
+  ENDIF()
+  
+  IF(Coin_LIBRARY_DEBUG AND Coin_LIBRARY_RELEASE)
+    IF(CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
+      #If the generator supports configuration types then set
+      #optimized and debug libraries, or if the CMAKE_BUILD_TYPE has a value
+      SET(Coin_LIBRARY optimized ${Coin_LIBRARY_RELEASE} debug ${Coin_LIBRARY_DEBUG})
+    ELSE()
+      #If there are no configuration types and CMAKE_BUILD_TYPE has no value
+      #then just use the release libraries
+      SET(Coin_LIBRARY ${Coin_LIBRARY_RELEASE})
+    ENDIF()    
+    SET(Coin_LIBRARIES optimized ${Coin_LIBRARY_RELEASE} debug ${Coin_LIBRARY_DEBUG})
+  ENDIF()
+
+  SET(Coin_LIBRARY ${Coin_LIBRARY} CACHE FILEPATH "The Coin library")  
+  MARK_AS_ADVANCED(Coin_LIBRARY_RELEASE Coin_LIBRARY_DEBUG)
+  
+  FIND_PATH(Coin_INCLUDE_DIR Inventor/SbLinear.h PATHS ${COINDIR} PATH_SUFFIXES include . NO_DEFAULT_PATH)
   
   IF (Coin_INCLUDE_DIR AND Coin_LIBRARY)
      SET(Coin_FOUND TRUE)
-  ENDIF (Coin_INCLUDE_DIR AND Coin_LIBRARY)
+  ENDIF ()
   
   IF (Coin_FOUND)
      IF (NOT Coin_FIND_QUIETLY)
@@ -38,7 +55,7 @@ IF(COINDIR)
      ENDIF (NOT Coin_FIND_QUIETLY)
   ELSE (Coin_FOUND)
      IF (Coin_FIND_REQUIRED)
-        MESSAGE(FATAL_ERROR "Could not find Coin in ${COINDIR}.")
+        MESSAGE(FATAL_ERROR "Could not find Coin in ${COINDIR} - try changing the COINDIR environment variable.")
      ENDIF (Coin_FIND_REQUIRED)
   ENDIF (Coin_FOUND)
   
@@ -54,7 +71,7 @@ ELSE(COINDIR)
     pkg_search_module(Coin Coin)
     IF(Coin_FOUND)
       MESSAGE(STATUS "Coin located using pkg-config.")
-      SET(Coin_LIBRARY ${Coin_LIBRARIES})
+      SET(Coin_LIBRARY ${Coin_LIBRARIES} ${Coin_LDFLAGS})
       SET(Coin_INCLUDE_DIR ${Coin_INCLUDE_DIRS})
     ENDIF(Coin_FOUND)
   ENDIF(PKG_CONFIG_FOUND)
@@ -67,7 +84,6 @@ ELSE(COINDIR)
     IF (APPLE)
       FIND_LIBRARY(Ogl_LIBRARY OpenGL)
       SET(Coin_LIBRARY ${Coin_LIBRARY} ${Ogl_LIBRARY})
-      MESSAGE(STATUS "Wtf: " ${Coin_LIBRARY})
     ENDIF(APPLE)
     IF (Coin_INCLUDE_DIR AND Coin_LIBRARY)
        SET(Coin_FOUND TRUE)
@@ -86,17 +102,21 @@ ELSE(COINDIR)
   ENDIF (Coin_FOUND)
 ENDIF(COINDIR)
 
-# Coin_DEFINES
+# Coin_DEFINES - only on WIN32
 IF (Coin_FOUND AND WIN32)
-  IF(EXISTS ${Coin_INCLUDE_DIR}/../Coin.pc)
-    FILE(READ ${Coin_INCLUDE_DIR}/../Coin.pc CoinPC)
+  FIND_FILE(Coin_pc NAMES Coin.pc PATHS ${Coin_INCLUDE_DIR}/../ PATH_SUFFIXES . lib/pkgconfig/ NO_DEFAULT_PATH)
+  MARK_AS_ADVANCED(Coin_pc)
+  IF(EXISTS ${Coin_pc})
+    FILE(READ ${Coin_pc} CoinPC)
     IF (${CoinPC} MATCHES COIN_DLL)
+      MESSAGE(STATUS "Found Coin.pc with -DCOIN_DLL")
       SET(Coin_DEFINES -DCOIN_DLL)
     ELSE()
+      MESSAGE(STATUS "Found Coin.pc with -DCOIN_NOT_DLL")
       SET(Coin_DEFINES -DCOIN_NOT_DLL)
     ENDIF()
   ELSE()
-    MESSAGE(STATUS "Could not find Coin.pc, using -DCOIN_DLL. This may be wrong though...")
+    MESSAGE(STATUS "WARNING: Could not find Coin.pc, using -DCOIN_DLL. This may be wrong though...")
     # Cross your fingers...
     SET(Coin_DEFINES -DCOIN_DLL)
   ENDIF()
