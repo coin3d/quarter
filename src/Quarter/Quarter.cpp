8e56e0c99b1728ef052270a2d716a2f4bc45d337
Tom Fredrik Blenning Klaussen <bfg@sim.no>
2008-06-16 05:55:39 +0000

    Clean our house, so we may live and die another day.

--- src/Quarter/Quarter.cpp
+++ src/Quarter/Quarter.cpp
@@ -1,7 +1,7 @@
 /**************************************************************************\
  *
  *  This file is part of the SIM Quarter extension library for Coin.
- *  Copyright (C) 1998-2007 by Systems in Motion.  All rights reserved.
+ *  Copyright (C) 1998-2008 by Systems in Motion.  All rights reserved.
  *
  *  This library is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License ("GPL") version 2
@@ -146,6 +146,8 @@ Quarter::~Quarter()
     KeyboardHandlerP::keypadmap->clear();
     delete KeyboardHandlerP::keyboardmap;
     delete KeyboardHandlerP::keypadmap;
+    KeyboardHandlerP::keyboardmap = NULL;
+    KeyboardHandlerP::keypadmap = NULL;
   }
 }
 
