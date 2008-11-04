3ca92a884f5176ca48543172e0d8ecb1278ae202
Lars J. Aas <larsa@sim.no>
2008-11-04 13:55:17 +0000

    sync with structural changes

--- build/misc/install-headers.bat
+++ build/misc/install-headers.bat
@@ -8,12 +8,12 @@ copy /Y ..\..\include\Quarter\Basic.h %COINDIR%\include\Quarter\Basic.h >nul:
 copy /Y ..\..\include\Quarter\Quarter.h %COINDIR%\include\Quarter\Quarter.h >nul:
 copy /Y ..\..\include\Quarter\QuarterWidget.h %COINDIR%\include\Quarter\QuarterWidget.h >nul:
 
-copy /Y ..\..\include\Quarter\devices\DeviceManager.h %COINDIR%\include\Quarter\devices\DeviceManager.h >nul:
 copy /Y ..\..\include\Quarter\devices\DeviceHandler.h %COINDIR%\include\Quarter\devices\DeviceHandler.h >nul:
-copy /Y ..\..\include\Quarter\devices\KeyboardHandler.h %COINDIR%\include\Quarter\devices\KeyboardHandler.h >nul:
-copy /Y ..\..\include\Quarter\devices\MouseHandler.h %COINDIR%\include\Quarter\devices\MouseHandler.h >nul:
+copy /Y ..\..\include\Quarter\devices\InputDevice.h %COINDIR%\include\Quarter\devices\InputDevice.h >nul:
+copy /Y ..\..\include\Quarter\devices\Keyboard.h %COINDIR%\include\Quarter\devices\Keyboard.h >nul:
+copy /Y ..\..\include\Quarter\devices\Mouse.h %COINDIR%\include\Quarter\devices\Mouse.h >nul:
 
-copy /Y ..\..\include\Quarter\eventhandlers\EventManager.h %COINDIR%\include\Quarter\eventhandlers\EventManager.h >nul:
+copy /Y ..\..\include\Quarter\eventhandlers\EventFilter.h %COINDIR%\include\Quarter\eventhandlers\EventFilter.h >nul:
 copy /Y ..\..\include\Quarter\eventhandlers\EventHandler.h %COINDIR%\include\Quarter\eventhandlers\EventHandler.h >nul:
 copy /Y ..\..\include\Quarter\eventhandlers\DragDropHandler.h %COINDIR%\include\Quarter\eventhandlers\DragDropHandler.h >nul:
 
