627977e6cfa4ab0cfce1470ff31df7302f276e66
Lars J. Aas <larsa@sim.no>
2008-11-04 13:56:18 +0000

    sync with structural changes

--- build/misc/uninstall-headers.bat
+++ build/misc/uninstall-headers.bat
@@ -8,12 +8,12 @@ del %COINDIR%\include\Quarter\Basic.h
 del %COINDIR%\include\Quarter\Quarter.h
 del %COINDIR%\include\Quarter\QuarterWidget.h
 
-del %COINDIR%\include\Quarter\devices\DeviceManager.h
 del %COINDIR%\include\Quarter\devices\DeviceHandler.h
-del %COINDIR%\include\Quarter\devices\KeyboardHandler.h
-del %COINDIR%\include\Quarter\devices\MouseHandler.h
+del %COINDIR%\include\Quarter\devices\InputDevice.h
+del %COINDIR%\include\Quarter\devices\Keyboard.h
+del %COINDIR%\include\Quarter\devices\Mouse.h
 
-del %COINDIR%\include\Quarter\eventhandlers\EventManager.h
+del %COINDIR%\include\Quarter\eventhandlers\EventFilter.h
 del %COINDIR%\include\Quarter\eventhandlers\EventHandler.h
 del %COINDIR%\include\Quarter\eventhandlers\DragDropHandler.h
 
