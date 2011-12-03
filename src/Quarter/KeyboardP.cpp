2c404bdfc2e5c6eeecf4651feacab4fe83cc07a8
Marius Kintel <marius@kintel.net>
2011-12-03 16:20:21 +0100

    Updated license headers to BSD

--- src/Quarter/KeyboardP.cpp
+++ src/Quarter/KeyboardP.cpp
@@ -1,23 +1,33 @@
 /**************************************************************************\
- *
- *  This file is part of the SIM Quarter extension library for Coin.
- *  Copyright (C) 1998-2009 by Systems in Motion.  All rights reserved.
- *
- *  This library is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License ("GPL") version 2
- *  as published by the Free Software Foundation.  See the file COPYING
- *  at the root directory of this source distribution for additional
- *  information about the GNU GPL.
- *
- *  For using SIM Quarter with software that can not be combined with
- *  the GNU GPL, and for taking advantage of the additional benefits of
- *  our support services, please contact Systems in Motion about acquiring
- *  a Coin Professional Edition License.
- *
- *  See <URL:http://www.coin3d.org/> for more information.
- *
- *  Systems in Motion AS, Bygdøy allé 5, N-0257 Oslo, NORWAY. (www.sim.no)
- *
+ * Copyright (c) Kongsberg Oil & Gas Technologies AS
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 \**************************************************************************/
 
 #include "KeyboardP.h"
